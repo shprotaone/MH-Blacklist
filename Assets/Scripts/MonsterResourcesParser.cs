47da701833abac6f43023b23ad546bc7336532fb
shprotaone <shprotaone@gmail.com>
2025-05-13 16:32:32 +0200

    feat: complete resource list

--- Assets/Scripts/MonsterResourcesParser.cs
+++ Assets/Scripts/MonsterResourcesParser.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MonsterResourcesParser
 {
     private const string riseResourceFileName = "SourceListRise";
-    private const string worldResourceFileName = "SourceListWorldSourceListWorld";
+    private const string worldResourceFileName = "SourceListWorld";
 
     private List<MonsterResourceList> _riseLists = new ();
     private List<MonsterResourceList> _worldList = new ();
@@ -17,6 +17,50 @@ public class MonsterResourcesParser
     public void Initialize(AssetProvider assetProvider)
     {
         InitRiseResources(assetProvider);
+        InitWorldResources(assetProvider);
+    }
+
+    private void InitWorldResources(AssetProvider assetProvider)
+    {
+        var json = assetProvider.GetText(worldResourceFileName);
+        var resourceListRaw = JsonUtility.FromJson<MonsterResources>(json);
+        
+        for (int i = 0; i < resourceListRaw.data.Length; i++)
+        {
+            var resource = resourceListRaw.data[i];
+            MonsterResourceList resourceList = new MonsterResourceList(
+                resource.name, RankType.LOW, Lang.RU,
+                resource.lowRU.Split(";", StringSplitOptions.RemoveEmptyEntries));
+
+            _worldList.Add(resourceList);
+
+            resourceList = new MonsterResourceList(
+                resource.name, RankType.HIGH, Lang.RU,
+                resource.highRU.Split(";", StringSplitOptions.RemoveEmptyEntries));
+
+            _worldList.Add(resourceList);
+
+            resourceList = new MonsterResourceList(
+                resource.name, RankType.MASTER, Lang.RU, resource.mrRU.Split(";", StringSplitOptions.RemoveEmptyEntries));
+
+            _worldList.Add(resourceList);
+
+            resourceList = new MonsterResourceList(resource.name, RankType.LOW, Lang.ENG,
+                resource.lowENG.Split("", StringSplitOptions.RemoveEmptyEntries));
+
+            _worldList.Add(resourceList);
+
+            resourceList = new MonsterResourceList(
+                resource.name, RankType.HIGH, Lang.ENG,
+                resource.highENG.Split(";", StringSplitOptions.RemoveEmptyEntries));
+
+            _worldList.Add(resourceList);
+
+            resourceList = new MonsterResourceList(
+                resource.name, RankType.MASTER, Lang.ENG, resource.mrENG.Split(";", StringSplitOptions.RemoveEmptyEntries));
+
+            _worldList.Add(resourceList);
+        }
     }
 
     private void InitRiseResources(AssetProvider assetProvider)
@@ -67,15 +111,24 @@ public class MonsterResourcesParser
         if (model.style == StyleType.RISE)
         {
             var monsterList = RiseList.FindAll(x => x.Key == model.name);
-            var rankList = monsterList.FindAll(x => x.RankType == model.rank);
+            var rank = model.rank;
+
+            if (rank == RankType.TEMPERED) rank = RankType.MASTER;
+            var rankList = monsterList.FindAll(x => x.RankType == rank);
+            
             var langList = rankList.Find(x => x.Lang == lang);
             return langList.Resources;
         }
         
+        
         if (model.style == StyleType.WORLD)
         {
             var monsterList = WorldList.FindAll(x => x.Key == model.name);
-            var rankList = monsterList.FindAll(x => x.RankType == model.rank);
+            var rank = model.rank;
+
+            if (rank == RankType.TEMPERED) rank = RankType.MASTER;
+            var rankList = monsterList.FindAll(x => x.RankType == rank);
+            
             var langList = rankList.Find(x => x.Lang == lang);
             return langList.Resources;
         }
